cb8dabc595ffe810e9cf901f97c0f4849e3d54c2
Jonas Sharron desktop 202 <jfsharron@msn.com>
2020-12-15 16:13:18 -0800

    first commit

---
+++ CH3_wage_calculatror.c
@@ -0,0 +1,10 @@
+//  ============================================================================
+//  Program:                CH3_wage_calculator
+//  File:                   CH3_wage_calculator.c
+//  Software Engineer:      Jonas Sharron
+//  Date:                   25-September-2018
+//  Purpose:                Program which determines gross pay (calculate both
+//                          standard pay and overtime) with output sent to 
+//                          screen and file.  Employee data is stored in an
+//                          array(s).
+//  ============================================================================
